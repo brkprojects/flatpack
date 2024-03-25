4b1346b81fa51bcf5269320375a4618a7399fdd2
ravikiranbotcha1702 <ravikiranbotcha1702@gmail.com>
2024-03-25 12:36:01 +0530

    upload

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../NavBar/NavBar.css'
+
+function NavBar() {
+  return (
+    <div class='contents'>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default NavBar
