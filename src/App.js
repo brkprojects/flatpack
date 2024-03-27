a95361bf33312cf2ad20728b4f358cc1d0d5a0c6
ravikiranbotcha1702 <ravikiranbotcha1702@gmail.com>
2024-03-27 08:44:36 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
+import LandingPage from './Components/LandingPage/LandingPage'
+import Branding from './Components/Branding/Branding'
+import CardItems from './Components/CardItems/CardItems'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <LandingPage />
+      <Branding />
+      <CardItems />
     </div>
   )
 }
