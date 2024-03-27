6bde4a52e1da156148c9441f00b4b3c897767e4b
ravikiranbotcha1702 <ravikiranbotcha1702@gmail.com>
2024-03-27 08:43:05 +0530

    Branding

---
+++ src/Components/Branding/Branding.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../Branding/Branding.css'
+import logo1 from '../Assets/logo1.png'
+import logo2 from '../Assets/logo2.png'
+import logo3 from '../Assets/logo3.png'
+
+function Branding() {
+    return (
+        <div class='logossection'>
+            <div class="textlogos">SOME OF THE WORLD'S MOST RESPECTED COMPANIES TRUST FLATPACK</div>
+
+            <div class="paddingupdown">
+            <img src={logo1}  height="38px" />
+            <img src={logo2}  height="38px" />
+            <img src={logo3}  height="38px" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Branding
